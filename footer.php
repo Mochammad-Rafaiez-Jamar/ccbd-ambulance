9a639e0f5c8b1996502e90bcd415be9b333f451f
Mochammad-Rafaiez-Jamar <127468517+Mochammad-Rafaiez-Jamar@users.noreply.github.com>
2024-12-04 02:59:46 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- ======= Footer ======= -->
+  <footer id="footer">
+    
+
+    <div class="container">
+      <div class="copyright">
+  <strong><span>Emergancy Ambulance Hiring Portal Copyright by <a href="https://www.youtube.com/@codecampbdofficial" target="_blank" rel="noopener noreferrer"></a></span></strong>.
+      </div>
+      
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
